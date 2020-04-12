2cfb0cac4103ebc31148cf4e3150fa273a0ee386
Giovanni <giovanni.zanca@studio.unibo.it>
2020-04-12 19:56:05 +0200

    .bat frontends + robots19

--- Sprint/SPRINT 11/it.unibo.frontend_definitivo/installFrontend.bat
+++ Sprint/SPRINT 11/it.unibo.frontend_definitivo/installFrontend.bat
@@ -1,3 +1,2 @@
-cd nodeCode
-cd frontend
+cd nodeCode/frontend
 npm install
---
+++ Sprint/SPRINT 11/startWebApplications.bat
@@ -0,0 +1,5 @@
+cd it.unibo.robots19
+start cmd /k startServer.bat
+cd ..
+cd it.unibo.frontend_definitivo
+start cmd /k startFrontend.bat
